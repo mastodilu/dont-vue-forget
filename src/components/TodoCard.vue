2aefdbb73175279a64717cc796383010d576cb67
mastodilu <matteo.dilucchio@edu.unito.it>
2020-04-30 17:57:56 +0200

    create card icons and delete todo cards locally

--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="todo-card">
+  <div class="todo-card" @mouseover='showIcons=true' @mouseleave="showIcons=false">
     <div class="card blue-grey darken-1">
+      <div v-if="showIcons">
+        <i class="small material-icons delete"  @click=actionDelete >  delete </i>
+        <i class="small material-icons edit"    @click=clicked  > edit    </i>
+      </div>
       <div class="card-content white-text">
         <p>{{todo.content}}</p>
         <span class="lime-text">{{todo.createdAt}}</span>
@@ -13,14 +17,48 @@
 export default{
   name: 'TodoCard',
   data: function(){
-    // variables here
+    return {
+      showIcons: false,
+      // variables here
+    }
   },
   props: ['todo'],
+  methods: {
+    clicked: function(){
+      console.log('clicked')
+    },
+    actionDelete: function() {
+      
+      this.$emit('deleteTodo', {createdAt:this.todo.createdAt})
+    }
+  }
 }
 </script>
 
 <style>
 .todo-card .card-content p {
   margin-bottom: 6px;
+}
+
+.todo-card i {
+  cursor: pointer;
+}
+
+.todo-card i.delete {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  color: tomato;
+}
+
+.todo-card i.edit {
+  position: absolute;
+  bottom: 4px;
+  right: 4px;
+  color: orange;
+}
+
+.card .card-content {
+  padding-right: 32px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <div class="container cards-container">
         <div v-for="(todo, index) in allTodos" :key=index>
-          <TodoCard :todo='todo' />
+          <TodoCard :todo='todo' @deleteTodo=deleteTodo />
       </div>
     </div>
   </div>
@@ -22,19 +21,27 @@ export default {
     return {
       allTodos: [
         {createdAt: '2020/04/30 15:46:23', content: 'Amet mollit sit occaecat ea amet commodo consequat et aliqua adipisicing.'},
-        {createdAt: '2020/04/30 15:46:23', content: 'Elit nisi excepteur fugiat id.'},
-        {createdAt: '2020/04/30 15:46:23', content: 'Aute voluptate mollit excepteur id esse excepteur.'},
-        {createdAt: '2020/04/30 15:46:23', content: 'Fugiat ex officia dolore adipisicing.'},
-        {createdAt: '2020/04/30 15:46:23', content: 'Lorem occaecat ut adipisicing velit fugiat anim excepteur magna incididunt cillum consequat.'},
-        {createdAt: '2020/04/30 15:46:23', content: 'Ut aliquip fugiat esse aute elit est mollit labore elit magna.'},
-        {createdAt: '2020/04/30 15:46:23', content: 'Consequat consectetur adipisicing aliqua ullamco cupidatat Lorem duis eu deserunt.'},
-        {createdAt: '2020/04/30 15:46:23', content: 'Id voluptate irure voluptate nisi minim officia dolor laboris qui ut ea magna incididunt.'},
-        {createdAt: '2020/04/30 15:46:23', content: 'Aliqua consequat pariatur cupidatat nulla.'},
-        {createdAt: '2020/04/30 15:46:23', content: 'Incididunt et est voluptate duis aliqua in aliqua elit consequat ea exercitation fugiat qui velit.'},
-        {createdAt: '2020/04/30 15:46:23', content: 'Do anim enim sit pariatur reprehenderit aute est mollit qui et laborum consequat.'},
+        {createdAt: '2020/04/29 15:46:23', content: 'Elit nisi excepteur fugiat id.'},
+        {createdAt: '2020/04/28 15:46:23', content: 'Aute voluptate mollit excepteur id esse excepteur.'},
+        {createdAt: '2020/04/27 15:46:23', content: 'Fugiat ex officia dolore adipisicing.'},
+        {createdAt: '2020/04/26 15:46:23', content: 'Lorem occaecat ut adipisicing velit fugiat anim excepteur magna incididunt cillum consequat.'},
+        {createdAt: '2020/04/25 15:46:23', content: 'Ut aliquip fugiat esse aute elit est mollit labore elit magna.'},
+        {createdAt: '2020/04/24 15:46:23', content: 'Consequat consectetur adipisicing aliqua ullamco cupidatat Lorem duis eu deserunt.'},
+        {createdAt: '2020/04/23 15:46:23', content: 'Id voluptate irure voluptate nisi minim officia dolor laboris qui ut ea magna incididunt.'},
+        {createdAt: '2020/04/22 15:46:23', content: 'Aliqua consequat pariatur cupidatat nulla.'},
+        {createdAt: '2020/04/21 15:46:23', content: 'Incididunt et est voluptate duis aliqua in aliqua elit consequat ea exercitation fugiat qui velit.'},
+        {createdAt: '2020/04/20 15:46:23', content: 'Do anim enim sit pariatur reprehenderit aute est mollit qui et laborum consequat.'},
       ]
     }
-  }
+  },
+  methods: {
+    deleteTodo: function(payload){
+      console.log("delete", payload)
+      this.allTodos = this.allTodos.filter(todo => {
+        return todo.createdAt !== payload.createdAt
+      })
+    }
+  },
 }
 </script>
 
