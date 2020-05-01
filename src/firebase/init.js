7ddca85f9844f6cba360a434e737f61316366b45
mastodilu <matteo.dilucchio@edu.unito.it>
2020-05-01 18:56:11 +0200

    add firebase and firestore

---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB2YQlAfvJ3kTsmgjf76scsZbnDgVSodNs",
+  authDomain: "dont-forget-20c3e.firebaseapp.com",
+  databaseURL: "https://dont-forget-20c3e.firebaseio.com",
+  projectId: "dont-forget-20c3e",
+  storageBucket: "dont-forget-20c3e.appspot.com",
+  messagingSenderId: "79693868177",
+  appId: "1:79693868177:web:8403c3ab8d01434c6fe330"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebaseApp.firestore().settings({timestampsInSnapshots: true})
+
+// export firestore database
+export default firebaseApp.firestore()
