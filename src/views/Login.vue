87a825370d017f3bceaf19c25c0b83f0d37b1cc2
matteo di lucchio <matteo.dilucchio@edu.unito.it>
2020-06-28 14:52:24 +0200

    create raw login form

---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="login">
+    <form @submit.prevent=submitForm>
+      <label for="email">Email:</label> <input type="text" id="email" name="email" v-model="email" required>
+      <label for="password">Password:</label> <input type="password" id="password" name="password" v-model="password" required>
+      <p class="alert">{{feedback}}</p>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data: function(){
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods:{ 
+    submitForm: function(){
+      feedback = ""
+      if(!email || email === '' || !password || password === ''){
+        feedback = "you need to complete each field"
+      }
+
+      feedback = "it's working"
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .alert {
+    color: red;
+  }
+</style>
