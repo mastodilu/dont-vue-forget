a4a85f8d2f60a7d892d31fee1bbd842e6a1fabd4
matteo di lucchio <matteo.dilucchio@edu.unito.it>
2020-06-28 19:22:12 +0200

    add routeguard to check authentication status

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import firebase from 'firebase'
 
 Vue.use(VueRouter)
 
@@ -25,6 +26,20 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  // check to see if route requires auth
+  if(to.meta && to.meta.requiresAuth){
+    let user = firebase.auth().currentUser
+    if(user){ // user signed in, proceed to route
+      next()
+    }else{ // not signed in, redirect to login
+      next({name:'Login'})
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
